2e0cfd6dd80d3097a0d18f38472eab609a58ac9a
sovisrushain <sovisrushain@gmail.com>
2021-02-26 15:40:27 +0530

    Fix bugs for production

--- src/App.js
+++ src/App.js
@@ -2,49 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import  Logo  from './assets/joker.png';
+import Logo from './assets/joker.png';
 import Card from './components/card';
 import { toast } from "react-toastify";
 
 class App extends Component {
-  state = { 
-    setUp : '',
-    punchline : ''
-   }
+  state = {
+    setUp: '',
+    punchline: ''
+  }
 
-   async componentDidMount() {
+  async componentDidMount() {
     const response = await fetch('https://official-joke-api.appspot.com/random_joke');
     const obj = await response.json();
-     this.setState({
-       setUp : obj.setup,
-       punchline : obj.punchline
-     })
-   }
+    this.setState({
+      setUp: obj.setup,
+      punchline: obj.punchline
+    })
+  }
 
-  render() { 
-    return ( 
+  render() {
+    return (
       <div className='container-fluid'>
-          <ToastContainer />
+        <ToastContainer />
         <div className='row'>
-          <img className='mx-auto mt-5' src={Logo} alt='logo' />
+          <img className='mx-auto mt-3' src={Logo} alt='logo' />
         </div>
-        <div className='row mt-4'>
+        <div className='row mt-3'>
           <h1 className='header mx-auto'>Laugh With Comedian</h1>
         </div>
         <div className='row'>
-          <Card data={this.state}/>
+          <Card data={this.state} />
         </div>
         <div className='row'>
-          <button onClick={this.handleClick} type="submit" className='btn btn-warning mx-auto m-2'><span>Next</span></button>
+          <button onClick={this.handleClick} type="button" className='btn btn-warning mx-auto mt-2'><span>Next</span></button>
         </div>
         <div className='row'>
-          <p className='footer mx-auto m-5'>2020 Cisco&copy;</p>
+          <p className='footer mx-auto pt-4'>2020 Cisco&copy;</p>
         </div>
-          <div className='row mt-0'>
-              <button onClick={this.handleAlert} type='button' className='rounded-circle btn btn-success btn-lg'>?</button>
-          </div>
+        <div className='row row-btn'>
+          <button onClick={this.handleAlert} type='button' className='rounded-circle btn btn-success btn-lg'>?</button>
+        </div>
       </div>
-     );
+    );
   }
 
   handleClick = () => {
@@ -52,9 +52,9 @@ class App extends Component {
   }
 
   handleAlert = () => {
-      toast.error('Hover Over Card For The Punchline');
+    toast.error('Hover Over Card For The Punchline');
   }
 
 }
- 
+
 export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -1,33 +1,33 @@
 import React from 'react';
 
 const Card = props => {
-    const{ setUp , punchline } = props.data;
-    return ( 
-        <div className="col-xs-12 col-sm-6 col-md-4 mx-auto mt-5">
-                <div className="image-flip" onTouchStart="this.classList.toggle('hover')">
-                    <div className="mainflip">
-                        <div className="frontside">
-                            <div className="card">
-                                <div className="card-body text-center">
-                                    <h1 className="card-title text-success">Setup</h1>
-                                    <p className=" line">-----------------------------------------------------------</p>
-                                    <p className="card-text">{setUp}</p>
-                                </div>
+    const { setUp, punchline } = props.data;
+    return (
+        <div className="col-xs-12 col-sm-6 col-md-4 mx-auto mt-3">
+            <div className="image-flip" onTouchStart="this.classList.toggle('hover')">
+                <div className="mainflip">
+                    <div className="frontside">
+                        <div className="card">
+                            <div className="card-body text-center">
+                                <h1 className="card-title text-success">Setup</h1>
+                                <p className=" line">-----------------------------------------------------------</p>
+                                <p className="card-text">{setUp}</p>
                             </div>
                         </div>
-                        <div className="backside">
-                            <div className="card">
-                                <div className="card-body text-center mt-4">
-                                    <h4 className="card-title">Punchline</h4>
-                                    <p className=" line">--------------------------------------------------------------</p>
-                                    <p className="card-text">{punchline}</p>
-                                </div>
+                    </div>
+                    <div className="backside">
+                        <div className="card">
+                            <div className="card-body text-center mt-4">
+                                <h4 className="card-title">Punchline</h4>
+                                <p className=" line">--------------------------------------------------------------</p>
+                                <p className="card-text">{punchline}</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-     );
+        </div>
+    );
 }
- 
+
 export default Card;
